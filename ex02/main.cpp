3639d6d89062a915dff8e7a7fee6345ed0a883d8
Евгения Костарева <eugenia@mac-book-air-2020.local>
2022-10-07 17:06:12 +0300

    add ex02

---
+++ ex02/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlarra <mlarra@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/06 11:43:18 by mlarra            #+#    #+#             */
+/*   Updated: 2022/10/07 17:04:32 by mlarra           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include <iostream>
+
+int	main(int ac, char **av)
+{
+	std::string	str = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &str;
+	std::string	&stringREF = str;
+
+	std::cout << "The memory address of the string: " << &str << std::endl;
+	std::cout << "The memory address held by stringPTR: " << stringPTR << std::endl;
+	std::cout << "The memory address held by stringREF: " << &stringREF << std::endl;
+
+	std::cout << "The value of the string : " << str << std::endl;
+	std::cout << "The value pointed to by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "The value pointed to by stringREF: " << stringREF << std::endl;
+	return (0);
+}
