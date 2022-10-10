9ddfee9ace3965e3d7e0cdfc8fddc5cf6d8dab9c
Евгения Костарева <eugenia@mac-book-air-2020.local>
2022-10-10 19:16:56 +0300

    fix 01

--- ex01/Zombie.cpp
+++ ex01/Zombie.cpp
@@ -6,7 +6,7 @@
 /*   By: mlarra <mlarra@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/06 11:45:09 by mlarra            #+#    #+#             */
-/*   Updated: 2022/10/07 21:55:03 by mlarra           ###   ########.fr       */
+/*   Updated: 2022/10/10 19:16:04 by mlarra           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,25 @@
 
 Zombie::Zombie(void)
 {
-	this->name = "";
+	this->Name = "";
 }
 
 Zombie::Zombie(std::string EnterName)
 {
-	this->name = EnterName;
+	this->Name = EnterName;
 }
 
 void	Zombie::set_name(std::string EnterName)
 {
-	this->name = EnterName;
+	this->Name = EnterName;
 }
 
 Zombie::~Zombie()
 {
-	std::cout << this->name << " is died" << std::endl;
+	std::cout << this->Name << " is died" << std::endl;
 }
 
 void	Zombie::announce( void )
 {
-	std::cout << this->name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+	std::cout << this->Name << ": BraiiiiiiinnnzzzZ..." << std::endl;
 }
